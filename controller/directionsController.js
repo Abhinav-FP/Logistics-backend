082d1bb629b86fe3c179c56df27929726b64a29a
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2025-01-24 16:55:00 +0530

    manage location

--- controller/directionsController.js
+++ controller/directionsController.js
@@ -8,28 +8,35 @@ exports.getDirections = async (req, res) => {
 
     try {
         const directionsUrl = `https://maps.googleapis.com/maps/api/directions/json?origin=${StartLocation}&destination=${EndLocation}&key=${apiKey}`;
-        const currentToStartUrl = `https://maps.googleapis.com/maps/api/directions/json?origin=${CurrentLocation}&destination=${StartLocation}&key=${apiKey}`;
+        const StartToCurrentUrl = `https://maps.googleapis.com/maps/api/directions/json?origin=${StartLocation}&destination=${CurrentLocation}&key=${apiKey}`;
+        const currentToEndUrl = `https://maps.googleapis.com/maps/api/directions/json?origin=${CurrentLocation}&destination=${EndLocation}&key=${apiKey}`;
 
 
 
         // Fetch data from Google Maps API
-        const [startToEndResponse, currentToStartResponse] = await Promise.all([
+        const [startToEndResponse, StartToCurrentResponse, currentToEndResponse] = await Promise.all([
             axios.get(directionsUrl),
-            axios.get(currentToStartUrl),
+            axios.get(StartToCurrentUrl),
+            axios.get(currentToEndUrl),
+
         ]);
 
-        if (startToEndResponse.data.status === 'OK' && currentToStartResponse.data.status === 'OK') {
+        if (startToEndResponse.data.status === 'OK' && StartToCurrentResponse.data.status === 'OK' && currentToEndResponse.data.status === 'OK') {
             const startToEndLeg = startToEndResponse.data.routes[0].legs[0];
-            const currentToStartLeg = currentToStartResponse.data.routes[0].legs[0];
-            const combinedPolyline = `${currentToStartResponse.data.routes[0].overview_polyline.points}|${startToEndResponse.data.routes[0].overview_polyline.points}`;
+            const StartToCurrentLeg = StartToCurrentResponse.data.routes[0].legs[0];
+            const currentToEndLeg = currentToEndResponse.data.routes[0].legs[0];
+            const combinedPolyline = `${StartToCurrentResponse.data.routes[0].overview_polyline.points}|${currentToEndResponse.data.routes[0].overview_polyline.points}|${startToEndResponse.data.routes[0].overview_polyline.points}`;
             const routeDetails = {
                 startToEndDistance: startToEndLeg.distance.text,
-                endToCurrentDistance: currentToStartLeg.distance.text,
                 startToEndDuration: startToEndLeg.duration.text,
-                endToCurrentDuration: currentToStartLeg.duration.text,
-                combinedPolyline: combinedPolyline,
                 startToEndPolyline: startToEndResponse.data.routes[0].overview_polyline.points,
-                currentToStartPolyline: currentToStartResponse.data.routes[0].overview_polyline.points,
+                StartToCurrentDistance: StartToCurrentLeg.distance.text,
+                StartToCurrentDuration: StartToCurrentLeg.duration.text,
+                StartToCurrentPolyline: StartToCurrentResponse.data.routes[0].overview_polyline.points,
+                currentToEndDistance: currentToEndLeg.distance.text,
+                currentToEndDuration: currentToEndLeg.duration.text,
+                currentToEndPolyline: currentToEndResponse.data.routes[0].overview_polyline.points,
+                combinedPolyline: combinedPolyline,
             };
 
 
@@ -58,4 +65,6 @@ exports.getDirections = async (req, res) => {
         res.status(500).json({ success: false, message: 'Internal Server Error' });
     }
 };
+
+
 
