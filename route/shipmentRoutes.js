54e58d6765a22743948d310af9157d84ee68fbf8
ankitfutureprofilez <ankit.jain@futureprofilez.com>
2025-02-04 14:34:55 +0530

    manage draw

--- route/shipmentRoutes.js
+++ route/shipmentRoutes.js
@@ -1,10 +1,12 @@
 const router = require("express").Router();
-const { updateShipment, getShipment, getBOL, createShipment, deleteShipment, getShipmentofBroker, getShipmentofCarrier } = require("../controller/shipmentController");
+const { updateShipment, getShipment, getBOL, createShipment, deleteShipment, getShipmentofBroker, getShipmentofCarrier, updateShipmentData } = require("../controller/shipmentController");
 const {checkPermission} = require('../middleware/rbacMiddleware');
 const {verifyToken} = require('../middleware/tokenVerify');
 
 router.post("/create",verifyToken, checkPermission('create-shipment'), createShipment);
 router.post("/update/:id",verifyToken, checkPermission('update-shipment'), updateShipment);
+router.post("/shipment_update/:id",verifyToken, checkPermission('update-shipment'), updateShipmentData);
+
 router.get("/delete/:id",verifyToken, checkPermission('update-shipment'), deleteShipment);
 router.get("/get/:id?",verifyToken, checkPermission('view-shipment'), getShipment);
 router.get("/get-shipment-broker",verifyToken, checkPermission('view-shipment'), getShipmentofBroker);
