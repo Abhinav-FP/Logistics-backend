5aa51ce9a608f74a4ac977ad50823eb3a36060a5
Abhinav-FP <a.mathur@futureprofilez.com>
2025-02-17 14:45:05 +0530

    Added ecosystemconfig

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    apps: [
+      {
+        name: 'Logistics-backend',
+        script: './App.js',
+        cwd: '/home/ubuntu/Logistics-backend',
+        env: {
+          NODE_ENV: 'production',
+        },
+      },
+    ],
+  };
